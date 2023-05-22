a3de2f5840503ca3066fb765fdf05c210d43bef2
Kajiane <kajianpulenthirasingam@gmail.comm>
2023-05-22 11:30:07 -0400

    Added error handling, fs module, and api routes

--- server.js
+++ server.js
@@ -1,29 +1,85 @@
 // Required modules
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
-// HTML Routes
-const note = require('./routes/notes.js')
-
-// Server to listen on a specified port
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 // Middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Home page route
+
+// HTML Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Notes page route
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-app.use("/api/notes", notes);
+// API Routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Error reading notes data' });
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
 
-app.listen(PORT, () => console.log(`Server listening port ${PORT}`));
+app.post('/api/notes', (req, res) => {
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Error reading notes data' });
+    }
+    const notes = JSON.parse(data);
+    const newNote = req.body;
+    notes.push(newNote);
+
+    fs.writeFile('db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error saving note' });
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Error reading notes data' });
+    }
+    const notes = JSON.parse(data);
+    // Find the index of the note with the specified ID
+    const noteIndex = notes.findIndex((note) => note.id === noteId);
+    if (noteIndex === -1) {
+      return res.status(404).json({ error: 'Note not found' });
+    }
+    // Remove the note from the array
+    notes.splice(noteIndex, 1);
+
+    fs.writeFile('db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error deleting note' });
+      }
+      res.sendStatus(204);
+    });
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
